81b00a91992614a7d3cad4b161591fd4103cf4f6
DEV <rafaelmunoz.prz@gmail.com>
2023-03-20 18:36:44 -0600

    0.0.0.4 -> Configuración del controlador, terminado

---
+++ app/Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Settings */
+require_once 'Config/Config.php';
+
+/* Home Page */
+require_once 'Models/Template.php';
+
+/* Sys Page */
+
+/* Libraries */
+require_once 'Library/Library.php';
